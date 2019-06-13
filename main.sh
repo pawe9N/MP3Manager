3f7e8b47f03de094eb70a671aa53b828b3b305ee
pawel <pawel@pc16.home>
2019-06-14 01:00:56 +0200

    Added getopts and some schema for future options

--- main.sh
+++ main.sh
@@ -1,34 +1,57 @@
 #!/bin/bash
 
-MainMenu(){
-dialog --backtitle "MP3Manager" \
-	--title "Main menu" \
-	--menu "Make your choice" 13 60 6 \
-	1 "Play song" \
-	2 "Exit" 2> .tempfile
-output=`cat .tempfile`
-echo $output
-rm -f .tempfile
+while getopts "vh" OPTION
+do
+    case $OPTION in
+		v ) echo "MP3Manager, version 1.0.0";;
+		h ) echo "To play song select 'Play song' option"
+		    echo "To see and edit MP3 tags select 'Show and edit file tags' option"
+		    echo "To download new MP3 files select 'Download MP3 files' option"
+		    echo "To quit the program select 'Exit' option";;
+    esac
+    exit 0
+done
 
-if [ "$output" = "1" ]; then
+ShowFiles(){
 	menuitems=()
 	while ((i++)); read song ;
 	do
 		menuitems+=( "$song" $i )
-	done < <(ls -A1 ../../Downloads/*.mp3)
+	done < <(find $HOME -iname "*.mp3" -print)
 
 	dialog --menu  "Make your own choice" 13 70 6 "${menuitems[@]}" 2>.tempfile
 	output=`cat .tempfile`
 	rm -f .tempfile
 	echo $output
 	clear
-	mpg123 "$output"
-	pid=$!
-	wait pid
-	MainMenu
-elif [ "$output" = "2" ]; then
-	clear
-fi
+}
+
+MainMenu(){
+	dialog --backtitle "MP3Manager" \
+		--title "Main menu" \
+		--menu "Make your choice" 13 60 6 \
+		1 "Play song" \
+		2 "Show and edit file tags" \
+		3 "Download MP3 files" \
+		4 "Exit" 2> .tempfile
+	output=`cat .tempfile`
+	echo $output
+	rm -f .tempfile
+	i=0
+	if [ "$output" = "1" ]; then
+		ShowFiles
+		mpg123 "$output"
+		pid=$!
+		wait pid
+		MainMenu
+	elif [ "$output" = "2" ]; then
+		ShowFiles
+		MainMenu
+	elif [ "$output" = "3" ]; then
+		clear
+	elif [ "$output" = "4" ]; then
+		clear
+	fi
 }
 MainMenu
 exit 0
